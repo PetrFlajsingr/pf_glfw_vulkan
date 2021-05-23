f81e1a6a0c1da3cda52619915711c4c9bcdc941b
petr <xflajs00@stud.fit.vutbr.cz>
2021-05-23 13:51:25 +0200

    fix logging for PhysicalDevice

--- src/pf_glfw_vulkan/logging.h
+++ src/pf_glfw_vulkan/logging.h
@@ -14,26 +14,26 @@ inline std::shared_ptr<ILogger> globalLogger = std::make_shared<EmptyLogger>();
 
 constexpr auto VK_TAG = "vulkan";
 
-inline void log(LogLevel level, std::string_view tag, std::string_view msg) {
-  details::globalLogger->log(level, tag, msg);
+inline void log(LogLevel level, std::string_view tag, std::string_view msg, const auto &...args) {
+  details::globalLogger->log(level, tag, fmt::format(msg, args...));
 }
 inline void logt(std::string_view tag, std::string_view msg, const auto &...args) {
-  log(LogLevel::Trace, tag, fmt::format(msg, args...));
+  log(LogLevel::Trace, tag, msg, args...);
 }
 inline void logi(std::string_view tag, std::string_view msg, const auto &...args) {
-  log(LogLevel::Info, tag, fmt::format(msg, args...));
+  log(LogLevel::Info, tag, msg, args...);
 }
 inline void logd(std::string_view tag, std::string_view msg, const auto &...args) {
-  log(LogLevel::Debug, tag, fmt::format(msg, args...));
+  log(LogLevel::Debug, tag, msg, args...);
 }
 inline void logw(std::string_view tag, std::string_view msg, const auto &...args) {
-  log(LogLevel::Warn, tag, fmt::format(msg, args...));
+  log(LogLevel::Warn, tag, msg, args...);
 }
 inline void logc(std::string_view tag, std::string_view msg, const auto &...args) {
-  log(LogLevel::Critical, tag, fmt::format(msg, args...));
+  log(LogLevel::Critical, tag, msg, args...);
 }
 inline void loge(std::string_view tag, std::string_view msg, const auto &...args) {
-  log(LogLevel::Err, tag, fmt::format(msg, args...));
+  log(LogLevel::Err, tag, msg, args...);
 }
 
 }// namespace pf::vulkan::logging
